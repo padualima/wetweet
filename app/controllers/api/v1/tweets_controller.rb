7c5db88de50a68d75ca370899637c730740f3e38
Pádua Lima <padualima.dev@gmail.com>
2020-02-11 16:09:39 -0300

    controller tweet with tests

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,19 +1,56 @@
-class Api::V1::TweetsController < ApplicationController
-  def index
-  end
+module Api
+  module V1
+    class TweetsController < Api::V1::ApiController
+      before_action :set_current_user
+      before_action :set_tweet, except: %i[create index]
+      before_action :authenticate_user, except: [:show, :index]
+      load_and_authorize_resource except: %i[index show create]
 
-  def show
-    @tweet = Tweet.last
-    render json: @tweet
-  end
+      def index
+        user = User.find params[:user_id]
+        @tweets = user.tweets.paginate(page: params[:page] || 1)
+        render json: @tweets
+      end
 
-  def create
+      def create
+        @tweet = Tweet.new(tweet_params.merge(user: current_user))
 
-  end
+        if @tweet.save
+          render json: @tweet, status: :created
+        else
+          render json: { errors: @tweet.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
 
-  def destroy
-  end
+      def show
+        render json: @tweet
+      end
 
-  def update
+      def destroy
+        @tweet.destroy
+      end
+
+      def update
+        if @tweet.update(tweet_params)
+          render json: @tweet
+        else
+          render json: { errors: @tweet.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_tweet
+        @tweet = Tweet.find(params[:id])
+      end
+
+      def tweet_params
+        params.require(:tweet).permit(:body, :tweet_original_id)
+      end
+
+      def set_current_user
+        @current_user = (current_user)? current_user : nil
+      end
+    end
   end
 end
