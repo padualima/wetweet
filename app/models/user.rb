884a385de13d9670afae367c94ed99498caee0b5
Pádua Lima <padualima.dev@gmail.com>
2020-02-25 16:07:44 -0300

    included timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,12 @@ class User < ApplicationRecord
   acts_as_follower
   has_many :tweets, dependent: :destroy
 
+  def timeline
+    timeline = tweets.map { |tweet| tweet }
+    all_following.each do |user|
+      timeline += user.tweets.map { |tweet| tweet }
+    end
+    timeline.sort_by!(&:created_at).reverse
+  end
+
 end
